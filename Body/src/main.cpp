20e1d01b28818793b10cbb4d6830129d8dd955de
Simon Westlin Green <simon.green@live.se>
2022-11-16 21:03:44 +0100

    Need to test it on bare metal now

--- Body/src/main.cpp
+++ Body/src/main.cpp
@@ -1,16 +1,16 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include "AsyncUDP.h"
 
 const char *ssid = "BB8_Master";
 const char *password = "BB8_Master_7739376";
+const unsigned long baudRate = 115200;
 
 // Variables
 bool sendCmd = false;
 String slaveCmd = "0";
 String slaveState = "0";
 
-// WiFi stuff
+// WiFi stuff, this is specific to the Master, the slave needs a different IP address
 WiFiServer server(80);
 WiFiClient browser;
 IPAddress ip(192, 168, 1, 4);
@@ -20,16 +20,18 @@ IPAddress subnet(255, 255, 255, 0);
 void setup()
 {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  WiFi.mode(WIFI_AP_STA);
+  Serial.begin(baudRate);
+  WiFi.mode(WIFI_AP);
   WiFi.config(ip, gateway, subnet);
-  WiFi.softAP("BB8", "ThisIsAPassword123");
-  WiFi.localIP();
+  WiFi.softAP(ssid, password);
+  Serial.println(F("Started AP with SSID BB8_Master and PASSWORD BB8_Master_7739376"));
+  Serial.print(F("Waiting for connection from slave"));
+  while (WiFi.status() != WL_CONNECTED)
+    Serial.print(F("."));
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-
   delay(200);
 }
