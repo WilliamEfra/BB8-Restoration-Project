f214f108b156fa5b4038a4d962dc05a0e6a83fa3
Simon Westlin Green <simon.green@live.se>
2022-11-16 20:57:30 +0100

    Moving to wifi

--- Body/src/main.cpp
+++ Body/src/main.cpp
@@ -1,19 +1,35 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include "AsyncUDP.h"
+
+const char *ssid = "BB8_Master";
+const char *password = "BB8_Master_7739376";
+
+// Variables
+bool sendCmd = false;
+String slaveCmd = "0";
+String slaveState = "0";
+
+// WiFi stuff
+WiFiServer server(80);
+WiFiClient browser;
+IPAddress ip(192, 168, 1, 4);
+IPAddress gateway(192, 168, 1, 254);
+IPAddress subnet(255, 255, 255, 0);
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("Bluetooth Started! Ready to pair...");
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.config(ip, gateway, subnet);
+  WiFi.softAP("BB8", "ThisIsAPassword123");
+  WiFi.localIP();
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  /* while (!Bluetooth.isConnected())
-  {
-    SerialBT.connect("MasterBluetoothBB8");
-  } */
-  delay(20);
+
+  delay(200);
 }
