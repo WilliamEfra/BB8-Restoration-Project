8a273dab81fb37f4636fcefde1429602af7c6542
Simon Westlin Green <simon.green@live.se>
2022-11-19 22:44:27 +0100

    Reworking some stuff

--- Body/src/main.cpp
+++ Body/src/main.cpp
@@ -7,11 +7,18 @@ const unsigned long BAUDRATE = 115200;
 const int SERVER_PORT = 4080;
 
 WiFiServer TCPServer(SERVER_PORT);
+WiFiClient TCPClient;
 
 // WiFi stuff, this is specific to the Master
 IPAddress ip(192, 168, 1, 4);
 IPAddress gateway(192, 168, 1, 254);
 IPAddress subnet(255, 255, 255, 0);
+
+void setupAP();
+void reconnect();
+void getSlaveMessage();
+void setup();
+void loop();
 
 void setupAP()
 {
@@ -26,12 +33,28 @@ void setupAP()
     Serial.print(F("."));
     delay(1000);
   }
+  Serial.println(F("\nConnected to slave"));
+}
 
+void reconnect()
+{
+  Serial.println(F("Slave disconnected"));
+  Serial.print(F("Waiting for connection from slave"));
+  while (WiFi.softAPgetStationNum() == 0)
+  {
+    Serial.print(F("."));
+    delay(1000);
+  }
   Serial.println(F("\nConnected to slave"));
 }
 
 void getSlaveMessages()
 {
+  if (TCPClient.available())
+  {
+    char c = TCPClient.read();
+    Serial.print(c);
+  }
 }
 
 void setup()
@@ -45,20 +68,23 @@ void setup()
 void loop()
 {
   // put your main code here, to run repeatedly:
-  WiFiClient TCPClient = TCPServer.available();
+  TCPClient = TCPServer.available();
   if (TCPClient)
   {
     Serial.println(F("Client connected"));
     while (TCPClient.connected())
     {
-      if (TCPClient.available())
-      {
-        char c = TCPClient.read();
-        Serial.print(c);
-      }
+      getSlaveMessages();
     }
     Serial.println(F("Client disconnected"));
     TCPClient.stop();
   }
+  // Attempt reconnect to station if disconnected
+  if (!WiFi.softAPgetStationNum())
+  {
+    // TODO Add some code to stop any and all actions while disconnected
+    reconnect();
+  }
+
   delay(200);
 }
